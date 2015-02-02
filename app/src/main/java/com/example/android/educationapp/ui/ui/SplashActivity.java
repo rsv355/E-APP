42f7df4964a1b03b06680ec6a2d28ce30ebba84e
Krishna Patel <softeng.krishna@gmail.com>
2015-02-02 16:56:12 +0530

    ccm

--- app/src/main/java/com/example/android/educationapp/ui/ui/SplashActivity.java
+++ app/src/main/java/com/example/android/educationapp/ui/ui/SplashActivity.java
@@ -124,7 +124,6 @@ public class SplashActivity extends Activity {
                 }
             }
         });
-
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
