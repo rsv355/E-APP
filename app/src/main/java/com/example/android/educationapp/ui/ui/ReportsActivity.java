916b62ad235be324ddd885cd2fce8a6966441feb
Krishna Patel <softeng.krishna@gmail.com>
2015-04-03 19:34:31 +0530

    gh

--- app/src/main/java/com/example/android/educationapp/ui/ui/ReportsActivity.java
+++ app/src/main/java/com/example/android/educationapp/ui/ui/ReportsActivity.java
@@ -4,12 +4,15 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -18,6 +21,13 @@ import com.example.android.educationapp.R;
 import com.example.android.educationapp.ui.base.DBAdapter;
 import com.example.android.educationapp.ui.base.QuestionDetails;
 import com.example.android.educationapp.ui.model.CurrentTest;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ValueFormatter;
 
 import net.qiujuer.genius.widget.GeniusButton;
 
@@ -33,23 +43,15 @@ import lecho.lib.hellocharts.view.LineChartView;
 
 
 public class ReportsActivity extends ActionBarActivity {
-    private Toolbar toolbar;
-    private GeniusButton btnStart;
-    ProgressDialog dialog;
-    public static int i;
-    private EditText etPassword;
-    private EditText etTestid;
-    private ListView listview;
-    private TextView txtCounter;
-    private QuestionDetails ques_d;
-    public static ArrayList<QuestionDetails> Ques_det;
-    int tempParseSize=0;
-    lecho.lib.hellocharts.view.LineChartView chart;
-    private boolean hasAxes = true;
-    private boolean hasAxesNames = true;
+
+
     public List<PointValue> values;
-    public static  ArrayList<CurrentTest> resultList=new ArrayList<CurrentTest>();
-    DBAdapter db;
+      DBAdapter db;
+
+    protected BarChart mChart;
+    private ArrayList<CurrentTest> tests ;
+
+    private Toolbar toolbar;
 
 
     @Override
@@ -72,19 +74,121 @@ public class ReportsActivity extends ActionBarActivity {
                 finish();
             }
         });
-        chart = (lecho.lib.hellocharts.view.LineChartView)findViewById(R.id.chart);
+
+        mChart = (BarChart) findViewById(R.id.chart1);
+
+        mChart.setDrawValuesForWholeStack(true);
+        mChart.setDrawBarShadow(false);
+        mChart.setDrawValueAboveBar(false);
+        mChart.setDescription("");
+        // if more than 60 entries are displayed in the chart, no values will be
+        // drawn
+        mChart.setMaxVisibleValueCount(60);
+        // scaling can now only be done on x- and y-axis separately
+        mChart.setPinchZoom(false);
+
+        // draw shadows for each bar that show the maximum value
+        //  mChart.setDrawBarShadow(true);
+        //   mChart.setDrawXLabels(false);
+        mChart.setDrawGridBackground(false);
+
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        xAxis.setTextSize(18f);
+        xAxis.setDrawGridLines(false);
+        xAxis.setSpaceBetweenLabels(2);
+
+
+      //  ValueFormatter custom = new MyValueFormatter();
+
+        YAxis leftAxis = mChart.getAxisLeft();
+
+        leftAxis.setLabelCount(8);
+    //    leftAxis.setValueFormatter(custom);
+        leftAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+        leftAxis.setSpaceTop(15f);
+
+        YAxis rightAxis = mChart.getAxisRight();
+        rightAxis.setDrawGridLines(false);
+
+        rightAxis.setLabelCount(8);
+  //      rightAxis.setValueFormatter(custom);
+        rightAxis.setSpaceTop(15f);
+
+
+
+
+        new fetchLevels().execute();
 
     }
 
+
+    private void updatePreferences() {
+
+       // String color = Prefs.with(TestListActivity.this).getString("back", "#494949");
+//        globalImageview.setBackgroundColor(Color.parseColor("#494949"));
+    }
+
+
+    private void setData(int count, float range) {
+
+        ArrayList<String> xVals = new ArrayList<String>();
+        for (int i = 0; i < count; i++) {
+            //xVals.add(mMonths[i % 12]);
+            float percentage = 100;
+            float val = tests.get(i).Correct_ques * percentage;
+            float val2 = val/tests.get(i).Total_ques;
+
+
+             xVals.add(String.format(tests.get(i).Test_id));
+
+
+
+        }
+        ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
+
+        for (int i = 0; i < count; i++) {
+          // Test test = tests.get(i);
+            float percentage = 100;
+
+            float val = tests.get(i).Correct_ques * percentage;
+            float val2 = val/tests.get(i).Total_ques;
+
+                if(val2==0){
+
+                    yVals1.add(new BarEntry(-10, i));
+                }else {
+                    yVals1.add(new BarEntry(val2, i));
+                }
+
+
+        }
+
+        BarDataSet set1 = new BarDataSet(yVals1, "Tests");
+        set1.setBarSpacePercent(40f);
+
+
+        ArrayList<BarDataSet> dataSets = new ArrayList<BarDataSet>();
+        dataSets.add(set1);
+
+        BarData data = new BarData(xVals, dataSets);
+//        data.setValueFormatter(new MyValueFormatter());
+        data.setValueTextSize(10f);
+        mChart.setData(data);
+
+    }
+
+
     void loaddata(){
-        resultList = new ArrayList<CurrentTest>();
+        tests = new ArrayList<CurrentTest>();
         db.open();
         Cursor c = db.getAllResult();
         if (c.moveToFirst()) {
             Display(c);
         }
         else {
-            resultList = new ArrayList<CurrentTest>();
+            tests = new ArrayList<CurrentTest>();
         }
         c.close();
         db.close();
@@ -108,83 +212,67 @@ public class ReportsActivity extends ActionBarActivity {
             newObj.Correct_ques = c.getInt(c.getColumnIndex("Correct_ques"));
             newObj.Wrong_ques = c.getInt(c.getColumnIndex("Wrong_ques"));
 
-            resultList.add(newObj);
+            tests.add(newObj);
             c.moveToNext();
         }
 
 
-         values = new ArrayList<PointValue>();
+
 
         // (3,6)  ==> 6 is the marks on x axis,,,,,,3 is the marks on y axis
 
-        for(int i=0;i<resultList.size();i++){
 
-            values.add(new PointValue(i, resultList.get(i).Correct_ques));
-
-        }
 
     }
+
+    private class fetchLevels extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            loaddata();
+            fillTests();
+        }
+    }
+
 
     @Override
     protected void onResume() {
         super.onResume();
 
 
-        loaddata();
 
-     /*   List<PointValue> values = new ArrayList<PointValue>();
-
-        // (3,6)  ==> 6 is the marks on x axis,,,,,,3 is the marks on y axis
-
-        for(int i=0;i<resultList.size();i++){
-
-            values.add(new PointValue(i, resultList.get(i).Correct_ques));
-
-        }
-*/
-
-
-/*
-        values.add(new PointValue(0, 10));
-        values.add(new PointValue(1, 15));
-        values.add(new PointValue(2, 9));*/
-
-
-        //In most cased you can call data model methods in builder-pattern-like manner.
-        Line line = new Line(values).setColor(Color.BLUE).setShape(ValueShape.CIRCLE).setCubic(true);
-        List<Line> lines = new ArrayList<Line>();
-        lines.add(line);
-
-
-
-        LineChartData data = new LineChartData();
-        data.setLines(lines);
-
-    //    chart.setLineChartData(data);
-
-
-        if (hasAxes) {
-            Axis axisX = new Axis();
-            Axis axisY = new Axis().setHasLines(true);
-            if (hasAxesNames) {
-                axisX.setName("Test ID");
-                axisY.setName("Marks");
-
-
-            }
-
-            data.setAxisXBottom(axisX);
-            data.setAxisYLeft(axisY);
-        } else {
-            data.setAxisXBottom(null);
-            data.setAxisYLeft(null);
-        }
-
-        data.setBaseValue(Float.NEGATIVE_INFINITY);
-        chart.setLineChartData(data);
+        updatePreferences();
 
 
     }
+
+    private void fillTests() {
+
+        if(tests == null || tests.isEmpty()){
+           /* setData(3, 100);
+            mChart.animateXY(1500, 1500);*/
+        }else{
+            setData(tests.size(), 100);
+            mChart.animateXY(1500, 1500);
+        }
+
+        //  TestAdapter adapter = new TestAdapter(TestListActivity.this,tests);
+        //  lvTests.setAdapter(adapter);
+
+    }
+
 
     //end of min class
 }
